73596d3cf01450e51def257419d3967384804ff6
Jonathan Pelletier <jopela@hedgenet.ca>
2018-06-19 12:29:24 -0400

    Adding partial payment code

--- bridge/payment.py
+++ bridge/payment.py
@@ -7,6 +7,21 @@ WALLET_RPC_PORT = 28888
 WALLET_RPC_HOST = '127.0.0.1'
 ORDER_ACCOUNT_INDEX = 1
 
+class Payment:
+
+    def __init__(self, daemon_host, daemon_port, order_account_index):
+        self.daemon_host = daemon_host
+        self.daemon_port = daemon_port
+        self.order_account_index = order_account_index
+
+        # Create SSH tunnel for communication with monero daemon
+        return
+
+    def create_pay_address(self, label=None):
+
+
+
+
 
 def create_pay_address(label=None):
 
